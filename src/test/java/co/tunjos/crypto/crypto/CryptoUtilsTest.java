6520e946e14aa8e417b274a10759fc0a982f6817
Tunji Olu-Taiwo <tunjos@tunjos.co>
2019-03-18 00:14:59 +0100

    CRYPTO: update CryptoUtilsTest
    - remove try-catch blocks

--- src/test/java/co/tunjos/crypto/crypto/CryptoUtilsTest.java
+++ src/test/java/co/tunjos/crypto/crypto/CryptoUtilsTest.java
@@ -17,195 +17,97 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CryptoUtilsTest {
 
     @Test
-    void generateAES256Key() {
+    void generateAES256Key() throws NoSuchAlgorithmException {
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAES256Key();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAES256Key();
 
         assertEquals("AES", secretKey.getAlgorithm());
         assertEquals(32, secretKey.getEncoded().length);
     }
 
     @Test
-    void generateAESKey256() {
+    void generateAESKey256() throws NoSuchAlgorithmException {
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_256);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_256);
 
         assertEquals("AES", secretKey.getAlgorithm());
         assertEquals(32, secretKey.getEncoded().length);
     }
 
     @Test
-    void generateAESKey192() {
+    void generateAESKey192() throws NoSuchAlgorithmException {
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_192);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_192);
 
         assertEquals("AES", secretKey.getAlgorithm());
         assertEquals(24, secretKey.getEncoded().length);
     }
 
     @Test
-    void generateAESKey128() {
+    void generateAESKey128() throws NoSuchAlgorithmException {
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_128);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAESKey(AesKeySizes.AES_128);
 
         assertEquals("AES", secretKey.getAlgorithm());
         assertEquals(16, secretKey.getEncoded().length);
     }
 
     @Test
-    void encryptAES256String() {
+    void encryptAES256String() throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException,
+            BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
         final String test = "test";
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAES256Key();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAES256Key();
 
         final byte[] initializationVector = new byte[16];
         new SecureRandom().nextBytes(initializationVector);
 
-
-        final String encryptedString;
-        try {
-            encryptedString = cryptoUtils.encryptAES256String(test, initializationVector, secretKey);
-        } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
-                InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final String encryptedString = cryptoUtils.encryptAES256String(test, initializationVector, secretKey);
 
         assertEquals(24, encryptedString.length());
     }
 
     @Test
-    void encryptBytesAES256String() {
+    void encryptBytesAES256String() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         final String test = "test";
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAES256Key();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAES256Key();
 
         final byte[] initializationVector = new byte[16];
         new SecureRandom().nextBytes(initializationVector);
 
-
-        final byte[] encryptedBytes;
-        try {
-            encryptedBytes = cryptoUtils.encryptBytesAES256String(test, initializationVector, secretKey);
-        } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
-                InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final byte[] encryptedBytes = cryptoUtils.encryptBytesAES256String(test, initializationVector, secretKey);
 
         assertEquals(16, encryptedBytes.length);
     }
 
     @Test
-    void encryptAES256Bytes() {
+    void encryptAES256Bytes() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         final String test = "test";
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAES256Key();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAES256Key();
 
         final byte[] initializationVector = new byte[16];
         new SecureRandom().nextBytes(initializationVector);
 
-        final byte[] encryptedBytes;
-        try {
-            encryptedBytes = cryptoUtils.encryptAES256Bytes(test.getBytes(StandardCharsets.UTF_8), initializationVector, secretKey);
-        } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
-                InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final byte[] encryptedBytes = cryptoUtils.encryptAES256Bytes(test.getBytes(StandardCharsets.UTF_8), initializationVector, secretKey);
 
         assertEquals(16, encryptedBytes.length);
     }
 
     @Test
-    void encryptDecryptAES256String() {
+    void encryptDecryptAES256String() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         final String test = "test";
         final CryptoUtils cryptoUtils = CryptoUtils.getInstance();
-        final SecretKey secretKey;
-        try {
-            secretKey = cryptoUtils.generateAES256Key();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final SecretKey secretKey = cryptoUtils.generateAES256Key();
 
         final byte[] initializationVector = new byte[16];
         new SecureRandom().nextBytes(initializationVector);
 
-        final String encryptedString;
-        try {
-            encryptedString = cryptoUtils.encryptAES256String(test, initializationVector, secretKey);
-        } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
-                InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final String encryptedString = cryptoUtils.encryptAES256String(test, initializationVector, secretKey);
 
-        final String decryptedString;
-        final byte[] decryptedBytes;
-        try {
-            decryptedString = cryptoUtils.decryptAES256String(encryptedString, initializationVector, secretKey);
-            decryptedBytes = cryptoUtils.decryptAES256Bytes(encryptedString.getBytes(StandardCharsets.UTF_8), initializationVector, secretKey);
-        } catch (NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
-                InvalidKeyException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-            assert false;
-            return;
-        }
+        final String decryptedString = cryptoUtils.decryptAES256String(encryptedString, initializationVector, secretKey);
+        final byte[] decryptedBytes = cryptoUtils.decryptAES256Bytes(encryptedString.getBytes(StandardCharsets.UTF_8), initializationVector, secretKey);
 
         assertEquals("test", decryptedString);
         assertEquals("test", new String(decryptedBytes, StandardCharsets.UTF_8));
