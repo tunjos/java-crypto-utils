a8dd16b08e151cd2e2053c620af8aa23d3795d64
Tunji Olu-Taiwo <tunjos@tunjos.co>
2019-03-17 19:30:57 +0100

    HASH: Update EncodingUtils
    - Add final to method params

--- src/main/java/co/tunjos/crypto/encoding/EncodingUtils.java
+++ src/main/java/co/tunjos/crypto/encoding/EncodingUtils.java
@@ -8,6 +8,12 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+/**
+ * Java Encoding Utilities.
+ *
+ * @author tunjos
+ * @version 1.0
+ */
 public class EncodingUtils {
     private static EncodingUtils ourInstance = new EncodingUtils();
 
@@ -19,38 +25,38 @@ public class EncodingUtils {
     }
 
     @NotNull
-    public String based64EncodeBytes(@NotNull byte[] bytes) {
+    public String based64EncodeBytes(@NotNull final byte[] bytes) {
         final Base64.Encoder base64Encoder = Base64.getEncoder();
         return base64Encoder.encodeToString(bytes);
     }
 
     @NotNull
-    public String based64EncodeString(@NotNull String input) {
+    public String based64EncodeString(@NotNull final String input) {
         final Base64.Encoder base64Encoder = Base64.getEncoder();
         return base64Encoder.encodeToString(input.getBytes());
     }
 
     @NotNull
-    public String based64DecodeBytes(@NotNull byte[] bytes) {
+    public String based64DecodeBytes(@NotNull final byte[] bytes) {
         final Base64.Decoder base64Decoder = Base64.getDecoder();
         byte[] decodedBytes = base64Decoder.decode(bytes);
         return new String(decodedBytes);
     }
 
     @NotNull
-    public String based64DecodeString(@NotNull String input) {
+    public String based64DecodeString(@NotNull final String input) {
         final Base64.Decoder base64Decoder = Base64.getDecoder();
         byte[] decodedBytes = base64Decoder.decode(input.getBytes());
         return new String(decodedBytes);
     }
 
     @NotNull
-    public String urlEncodeString(@NotNull String input) throws UnsupportedEncodingException {
+    public String urlEncodeString(@NotNull final String input) throws UnsupportedEncodingException {
         return URLEncoder.encode(input, StandardCharsets.UTF_8.name());
     }
 
     @NotNull
-    public String urlDecodeString(@NotNull String input) throws UnsupportedEncodingException {
+    public String urlDecodeString(@NotNull final String input) throws UnsupportedEncodingException {
         return URLDecoder.decode(input, StandardCharsets.UTF_8.name());
     }
 }
